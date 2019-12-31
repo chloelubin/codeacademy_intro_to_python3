a0812f2f02a9fd1ebad51b689c5f15f0e4dd61d4
Chloe Lubin <chloe.lubin@gmail.com>
2019-12-31 11:44:11 -0800

    Reformatted files & edited code

--- menu_franchise_challenge.py
+++ menu_franchise_challenge.py
@@ -1,106 +1,128 @@
-#create parent class Menu
+# create parent class Menu
 class Menu:
-  def __init__(self, name, items, start_time, end_time):
-    self.name = name 
-    self.items = items
-    self.start_time = start_time
-    self.end_time = end_time
+    def __init__(self, name, items, start_time, end_time):
+        self.name = name
+        self.items = items
+        self.start_time = start_time
+        self.end_time = end_time
 
-  #define a string representation to inspect the object
-  def __repr__(self):
-    return '{name} menu available from {start_time} to {end_time}'.format(name = self.name, start_time = self.start_time, end_time = self.end_time)
-  
-  #define a function to calculate the bill based on menu items purchased
-  def calculate_bill(self, purchased_items):
-    price_sum = 0
-    for item in purchased_items:
-      if item in self.items:
-        value_get = self.items.get(item)
-        price_sum += value_get
-    return price_sum
-  
-#create sub-class Brunch that inherits from Menu
+    # define a string representation to inspect the object
+    def __repr__(self):
+        return '{name} menu available from {start_time} to {end_time}'.format(name=self.name,
+                                                                              start_time=self.start_time,
+                                                                              end_time=self.end_time)
+
+    # define a function to calculate the bill based on menu items purchased
+    def calculate_bill(self, purchased_items):
+        price_sum = 0
+        for item in purchased_items:
+            if item in self.items:
+                value_get = self.items.get(item)
+                price_sum += value_get
+        return price_sum
+
+
+# create sub-class Brunch that inherits from Menu
 class Brunch(Menu):
-  def __init__(self, name, items, start_time, end_time):
-    super().__init__(name, items, start_time, end_time)
+    def __init__(self, name, items, start_time, end_time):
+        super().__init__(name, items, start_time, end_time)
 
-brunch = Brunch('brunch', {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}, 11, 16)
-   
+
+brunch = Brunch('brunch', {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50,
+                           'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}, 11, 16)
 
 print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
 
-#create sub-class Early_Bird that inherits from Menu
+
+# create sub-class Early_Bird that inherits from Menu
 class Early_Bird(Menu):
-  def __init__(self, name, items, start_time, end_time):
-    super().__init__(name, items, start_time, end_time)
-    
-early_bird = Early_Bird('early_bird', {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00}, 15, 18)
+    def __init__(self, name, items, start_time, end_time):
+        super().__init__(name, items, start_time, end_time)
+
+
+early_bird = Early_Bird('early_bird', {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00,
+                                       'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50,
+                                       'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00}, 15, 18)
 
 print(early_bird.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
 
-#create sub-class Dinner that inherits from Menu
+
+# create sub-class Dinner that inherits from Menu
 class Dinner(Menu):
-  def __init__(self, name, items, start_time, end_time):
-    super().__init__(name, items, start_time, end_time)  
-    
-dinner = Dinner('dinner', {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00}, 17, 23)
-    
-#create sub-class Kids that inherits from Menu
+    def __init__(self, name, items, start_time, end_time):
+        super().__init__(name, items, start_time, end_time)
+
+
+dinner = Dinner('dinner',
+                {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00,
+                 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00}, 17, 23)
+
+
+# create sub-class Kids that inherits from Menu
 class Kids(Menu):
-  def __init__(self, name, items, start_time, end_time):
-    super().__init__(name, items, start_time, end_time)
-  
+    def __init__(self, name, items, start_time, end_time):
+        super().__init__(name, items, start_time, end_time)
+
+
 kids = Kids('kids', {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, 11, 21)
 
 """
 create sub-class Arepa that inherits from Menu (this child class is a little different: 
 Arepa is one of the franchises but has a menu that's unique, which is why we are giving it a class of its own)
 """
+
+
 class Arepa(Menu):
-  def __init__(self, name, items, start_time, end_time):
-    super().__init__(name, items, start_time, end_time)
+    def __init__(self, name, items, start_time, end_time):
+        super().__init__(name, items, start_time, end_time)
+
 
 arepas_menu = Arepa('Take a’ Arepa', {
-  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+    'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
 }, 10, 20)
 
 menus = [brunch, early_bird, dinner, kids]
 
-#create sub-class Franchise that inherits from Menu
-class Franchise(Menu):
-  def __init__(self, address, menus):
-    self.address = address
-    self.menus = menus
 
-  #define a string representation to inspect the object
-  def __repr__(self):
-    print('The address of the Franchise is {address}'.format(address = self.address))
-  
-  #define a function to return a list of menu items available at a given time, which the user specifies when calling the function
-  def available_menus(self, time):
-    available_menu_list = []
-    for menu in self.menus:
-      if time >= menu.start_time and time <= menu.end_time:
-        available_menu_list.append(menu)
-      return available_menu_list
+# create sub-class Franchise that inherits from Menu
+class Franchise(Menu):
+    def __init__(self, address, menus):
+        self.address = address
+        self.menus = menus
+
+    # define a string representation to inspect the object
+    def __repr__(self):
+        print('The address of the Franchise is {address}'.format(address=self.address))
+
+    # define a function to return a list of menu items available at a given time, which the user specifies when
+    # calling the function
+    def available_menus(self, time):
+        available_menu_list = []
+        for menu in self.menus:
+            if menu.start_time <= time <= menu.end_time:
+                available_menu_list.append(menu)
+            return available_menu_list
+
 
 flagship_store = Franchise('1232 West End Road', menus)
 
-new_installment = Franchise('12 East Mulberry Street',menus)
+new_installment = Franchise('12 East Mulberry Street', menus)
 
 arepas_place = Franchise('189 Fitzgerald Avenue', arepas_menu)
 
-#prints available items at 5 p.m. at our flagship_store franchise
+# prints available items at 5 p.m. at our flagship_store franchise
 print(flagship_store.available_menus(17))
 
-#create sub-class Business that inherits from Franchise
+
+# create sub-class Business that inherits from Franchise
 class Business(Franchise):
-  def __init__(self, name, franchises):
-    self.name = name
-    self.franchises = franchises
-   
-#assign "Basta Fazoolin' with my Heart" as the business name for our flagship_store & new_installment franchises
+    def __init__(self, name, franchises):
+        self.name = name
+        self.franchises = franchises
+
+
+# assign "Basta Fazoolin' with my Heart" as the business name for our flagship_store & new_installment franchises
 business = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
 
-#assign "Take a' Arepa" as the business name for our arepas_place franchise
+# assign "Take a' Arepa" as the business name for our arepas_place franchise
 business2 = Business("Take a' Arepa", arepas_place)
--- shipping_method_challenge.py
+++ shipping_method_challenge.py
@@ -1,50 +1,52 @@
-#determine ground shipping cost
+# determine ground shipping cost
 def ground_ship(weight):
-  if weight <= 2:
-    return weight * 1.5 + 20.00
-  elif weight > 2 and weight <= 6:
-    return weight * 3.00 + 20.00
-  elif weight > 6 and weight <= 10:
-    return weight * 4 + 20.00
-  else:
-    return weight * 4.75 + 20.00
+    if 2 >= weight:
+        return weight * 1.5 + 20.00
+    elif 2 < weight <= 6:
+        return weight * 3.00 + 20.00
+    elif 6 < weight <= 10:
+        return weight * 4 + 20.00
+    else:
+        return weight * 4.75 + 20.00
 
-#test ground shipping cost function  
+
+# test ground shipping cost function
 print(ground_ship(100))
 
-#create a variable with a flat rate value
+# create a variable with a flat rate value
 premium_ground = 125.00
 
-#determine shipping cost by drone
+
+# determine shipping cost by drone
 def drone_ship(weight):
-  if weight <= 2:
-    return weight * 4.5 + 0
-  elif weight > 2 and weight <= 6:
-    return weight * 9.00 + 0
-  elif weight > 6 and weight <= 10:
-    return weight * 12 + 0
-  else:
-    return weight * 14.25 + 0
-  
-#test drone shipping cost function
+    if 2 >= weight:
+        return weight * 4.5 + 0
+    elif 2 < weight <= 6:
+        return weight * 9.00 + 0
+    elif 6 < weight <= 10:
+        return weight * 12 + 0
+    else:
+        return weight * 14.25 + 0
+
+
+# test drone shipping cost function
 print(drone_ship(1.5))
 
-#determine cheapest shipping method between: ground shipping, premium ground shipping, and drone shipping methods.
+
+# determine cheapest shipping method between: ground shipping, premium ground shipping, and drone shipping methods.
 def cheapest_shipping(weight):
-  is_ground_cheapest = ground_ship(weight) < premium_ground and ground_ship(weight) < drone_ship(weight)
-  
-  is_premium_cheapest = premium_ground < ground_ship(weight) and premium_ground < drone_ship(weight)
-        
-  is_drone_cheapest = drone_ship(weight) < ground_ship(weight) and drone_ship(weight) < premium_ground
-  
-  if is_ground_cheapest:
-    print("You should ship using ground shipping, it will cost "+str(ground_ship(weight)))
-  
-  elif is_premium_cheapest:
-    print("You should ship using premium ground shipping, it will cost "+str(premium_ground))
+    is_ground_cheapest = ground_ship(weight) < premium_ground and ground_ship(weight) < drone_ship(weight)
 
-  else:
-    print("You should ship using drone shipping, it will cost "+str(drone_ship(weight)))
+    is_premium_cheapest = premium_ground < ground_ship(weight) and premium_ground < drone_ship(weight)
 
-#test cheapest_shipping function
+    if is_ground_cheapest:
+        print('You should ship using ground shipping, it will cost ' + str(ground_ship(weight)))
+
+    elif is_premium_cheapest:
+        print('You should ship using premium ground shipping, it will cost ' + str(premium_ground))
+
+    else:
+        print('You should ship using drone shipping, it will cost ' + str(drone_ship(weight)))
+
+# test cheapest_shipping function
 print(cheapest_shipping(10))
